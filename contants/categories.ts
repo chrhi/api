c31f0ecd9824b6eded556b2c074a96ff7404c2a9
abdellah <Abdellah.chehri14@gmail.com>
2024-05-19 05:50:44 +0200

    add changes to github

---
+++ contants/categories.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+export const categries = [
+  {
+    type: 'shoes',
+    subcatigores: ['hieed', 'ejwiew'],
+  },
+  {
+    type: 'watches',
+  },
+  {
+    type: 'cars',
+  },
+];
